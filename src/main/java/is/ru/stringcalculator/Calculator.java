adb719890a11e6595bb20bf2c693d5b2cb17547b
Ásgeir Þór Másson <asgeirm14@ru.is>
2015-10-16 14:29:30 +0000

    Refactored - added findDelimiter function

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -17,27 +17,39 @@ public class Calculator {
 
 	private static String[] splitNumbers(String numbers){
 	    	if(numbers.charAt(0) == '/' && numbers.charAt(1) == '/'){
-			if(numbers.indexOf('[') != -1){
-				int indexOfClosingBracket;
-				indexOfClosingBracket = numbers.indexOf("]");
-				String deliminator = numbers.substring(3, indexOfClosingBracket);
-				StringBuilder delim = new StringBuilder("");
-				CharSequence escapeChars = "\\";
-				for(int i = 0; i < deliminator.length(); i++){
-					delim.append(escapeChars);
-					delim.append(deliminator.charAt(i));
-				}
-				String newNumbers = numbers.substring(indexOfClosingBracket +2, numbers.length());
+			String delimiter = findDelimiter(numbers);
+			StringBuilder delim = new StringBuilder("");
+			CharSequence escapeChars = "\\";
+			for(int i = 0; i < delimiter.length(); i++){
+				delim.append(escapeChars);
+				delim.append(delimiter.charAt(i));
+			}
+			if(delimiter.length() > 1){
+				String newNumbers = numbers.substring(numbers.indexOf("]") +2, numbers.length());
 				return newNumbers.split("(\\\n)|" + String.format("(%s)", delim));
 			}
 			else{
 				String newNumbers = numbers.substring(4, numbers.length());
-	                        return newNumbers.split("(\\\n)|" + String.format("(%c)", numbers.charAt(2)));
+	                        return newNumbers.split("(\\\n)|" + String.format("(%s)", delimiter));
 			}
 		} 
 		return numbers.split("(,)|(\\\n)");
 	}
-      
+
+	private static String findDelimiter(String numbers){
+		String delimiter = "";
+		if(numbers.indexOf('[') == -1){
+			char delim = numbers.charAt(2);
+			delimiter = "" + delim;
+		}
+		else{
+                        int indexOfClosingBracket;
+                        indexOfClosingBracket = numbers.indexOf("]");
+                        delimiter = numbers.substring(3, indexOfClosingBracket);
+		}
+		return delimiter;
+	}
+
 	private static int sum(String[] numbers){
  		int total = 0;
 		String message = "Negatives not allowed: ";
